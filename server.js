13d0e74ba258470727033791dead7b2da525f6c6
ksherbondy <k.sherbondy86@gmail.com>
2026-05-07 23:45:59 -0500

    initial project setup

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('views', './views');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(8080, () => {
+    console.log('listening at http://localhost:8080');
+});
+
